80ea754d764239358d3ad5be712ab8aec7d7b7fb
k2491p <fukuda.dev10@gmail.com>
2022-05-20 20:17:50 +0900

    add register.dart

---
+++ lib/presentation/page/register.dart
@@ -0,0 +1,52 @@
+import 'package:cracking_counter/application/shared.dart';
+import 'package:cracking_counter/presentation/component/footer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<Register> createState() => _Register();
+}
+
+class _Register extends State<Register> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'registration page:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Footer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
