b1fb8b75d14d4de9a54d769972c9188dca0fe920
k2491p <fukuda.dev10@gmail.com>
2022-06-09 21:52:06 +0900

    クエリ修正
    tree構造を表現

--- lib/infrastructure/sqlite/cracking_counter_sqlite.dart
+++ lib/infrastructure/sqlite/cracking_counter_sqlite.dart
@@ -5,7 +5,7 @@ import 'package:cracking_counter/infrastructure/sqlite/db_helper.dart';
 class CrackingCounterSqlite implements ICrackingCounterRepository {
   @override
   Future<List<CrackingCounterEntity>> getAll(String userId) async {
-    final List<Map<String, dynamic>> result = await DbHelper.db!.query('cracking_list_view', where: 'user_id = ?', whereArgs: [userId]);
+    final List<Map<String, dynamic>> result = await DbHelper.db!.query('cracking_count_view', where: 'user_id = ?', whereArgs: [userId]);
     return List.generate(result.length, (i) {
       return CrackingCounterEntity(result[i]['user_id'],result[i]['body_part_id'],result[i]['body_part_name'],result[i]['total_count'],result[i]['today_count']);
     });
--- lib/infrastructure/sqlite/db_helper.dart
+++ lib/infrastructure/sqlite/db_helper.dart
@@ -22,6 +22,18 @@ class DbHelper {
         var userId =uuid.v4().toString();
         const storage = FlutterSecureStorage();
         await storage.write(key: 'userId', value: userId);
+        var rightHand = '9e593b71-ec5e-89f0-4b82-4b08871ac9f4';
+        var rightThumb = 'fb5726dd-1b36-0ad8-04ad-887df360867f';
+        var rightIndex = '4f149f46-ec36-3689-0e52-44189ce5e61a';
+        var rightMiddle = '3e872f29-9e4e-19fb-f36d-96edb3767e60';
+        var rightRing = 'f0738bf5-48b7-1573-e21d-44dd30b30b11';
+        var rightLittle = '9cb4eb1e-9ec1-b1ac-ce43-3ae15c8b68e2';
+        var leftHand = 'b6075b51-7b8f-9b21-2056-e8938b481b80';
+        var leftThumb = '01a345a2-25e7-a09c-2bd0-04a3b691be7c';
+        var leftIndex = 'ed9f5bc8-7d62-2a63-0469-66cfd40aa2a1';
+        var leftMiddle = '0f90a92b-75b2-e438-5a68-cc15a24f6767';
+        var leftRing = '18704685-c9a9-fb2e-c475-56bf8b4f5cc6';
+        var leftLittle = '06f4dc50-22e5-48be-efd5-61371d1aee49';
 
         await db.execute('''
           CREATE TABLE user (
@@ -32,7 +44,7 @@ class DbHelper {
         ''');
 
         await db.execute('''
-          INSERT INTO "user" (id, name) VALUES('${userId}', '');
+          INSERT INTO "user" (id, name) VALUES('$userId', '');
         ''');
 
         await db.execute('''
@@ -49,42 +61,82 @@ class DbHelper {
           CREATE TABLE body_part (
             id TEXT NOT NULL,
             name TEXT NOT NULL,
+            display_order INTEGER NOT NULL,
             PRIMARY KEY (id)
           )
         ''');
 
         await db.execute('''
-          INSERT INTO body_part (id, name) VALUES('a50a395b-fdf4-e5ae-9751-2866a0c087cf', '首');
+          INSERT INTO body_part (id, name, display_order) VALUES('a50a395b-fdf4-e5ae-9751-2866a0c087cf', '首', 10);
         ''');
 
 
         await db.execute('''
-          INSERT INTO body_part (id, name) VALUES('56ce60f3-a267-cd00-de00-5a69d3c37900', '背中');
+          INSERT INTO body_part (id, name, display_order) VALUES('56ce60f3-a267-cd00-de00-5a69d3c37900', '背中', 20);
         ''');
 
 
         await db.execute('''
-          INSERT INTO body_part (id, name) VALUES('0e4f2659-724e-c93f-d709-ef4bee18ceb3', '腰');
+          INSERT INTO body_part (id, name, display_order) VALUES('0e4f2659-724e-c93f-d709-ef4bee18ceb3', '腰', 30);
         ''');
 
 
         await db.execute('''
-          INSERT INTO body_part (id, name) VALUES('9e593b71-ec5e-89f0-4b82-4b08871ac9f4', '右手');
+          INSERT INTO body_part (id, name, display_order) VALUES('$rightHand', '右手', 40);
+        ''');
+
+        await db.execute('''
+          INSERT INTO body_part (id, name, display_order) VALUES('$rightThumb', '親指', 41);
+        ''');
+
+        await db.execute('''
+          INSERT INTO body_part (id, name, display_order) VALUES('$rightIndex', '人差指', 42);
+        ''');
+
+        await db.execute('''
+          INSERT INTO body_part (id, name, display_order) VALUES('$rightMiddle', '中指', 43);
+        ''');
+
+        await db.execute('''
+          INSERT INTO body_part (id, name, display_order) VALUES('$rightRing', '薬指', 44);
+        ''');
+
+        await db.execute('''
+          INSERT INTO body_part (id, name, display_order) VALUES('$rightLittle', '小指', 45);
         ''');
 
 
         await db.execute('''
-          INSERT INTO body_part (id, name) VALUES('b6075b51-7b8f-9b21-2056-e8938b481b80', '左手');
+          INSERT INTO body_part (id, name, display_order) VALUES('$leftHand', '左手', 50);
+        ''');
+
+        await db.execute('''
+          INSERT INTO body_part (id, name, display_order) VALUES('$leftThumb', '親指', 51);
+        ''');
+
+        await db.execute('''
+          INSERT INTO body_part (id, name, display_order) VALUES('$leftIndex', '人差指', 52);
+        ''');
+
+        await db.execute('''
+          INSERT INTO body_part (id, name, display_order) VALUES('$leftMiddle', '中指', 53);
+        ''');
+
+        await db.execute('''
+          INSERT INTO body_part (id, name, display_order) VALUES('$leftRing', '薬指', 54);
+        ''');
+
+        await db.execute('''
+          INSERT INTO body_part (id, name, display_order) VALUES('$leftLittle', '小指', 55);
+        ''');
+
+        await db.execute('''
+          INSERT INTO body_part (id, name, display_order) VALUES('a2cc7c56-c5b9-4280-640f-7fc4b7e7bb02', '右足', 60);
         ''');
 
 
         await db.execute('''
-          INSERT INTO body_part (id, name) VALUES('a2cc7c56-c5b9-4280-640f-7fc4b7e7bb02', '右足');
-        ''');
-
-
-        await db.execute('''
-          INSERT INTO body_part (id, name) VALUES('2a12d9d5-2970-72fa-3bb5-087d64432ad0', '左足');
+          INSERT INTO body_part (id, name, display_order) VALUES('2a12d9d5-2970-72fa-3bb5-087d64432ad0', '左足', 70);
         ''');
 
         await db.execute('''
@@ -92,11 +144,16 @@ class DbHelper {
             id TEXT NOT NULL,
             ancestor_body_part_id TEXT NOT NULL,
             descendent_body_part_id TEXT NOT NULL,
-            name TEXT NOT NULL,
             PRIMARY KEY (id),
             FOREIGN KEY (ancestor_body_part_id) REFERENCES body_part(id),
             FOREIGN KEY (descendent_body_part_id) REFERENCES body_part(id)
           )
+        ''');
+
+        await db.execute('''
+          INSERT INTO body_part_tree (id, ancestor_body_part_id, descendent_body_part_id) 
+          VALUES ('${uuid.v4().toString()}', '$rightHand', '$rightThumb'), ('${uuid.v4().toString()}', '$rightHand', '$rightIndex'), ('${uuid.v4().toString()}', '$rightHand', '$rightMiddle'), ('${uuid.v4().toString()}', '$rightHand', '$rightRing'), ('${uuid.v4().toString()}', '$rightHand', '$rightLittle'),
+          ('${uuid.v4().toString()}', '$leftHand', '$leftThumb'), ('${uuid.v4().toString()}', '$leftHand', '$leftIndex'), ('${uuid.v4().toString()}', '$leftHand', '$leftMiddle'), ('${uuid.v4().toString()}', '$leftHand', '$leftRing'), ('${uuid.v4().toString()}', '$leftHand', '$leftLittle');
         ''');
 
         await db.execute('''
@@ -117,6 +174,8 @@ class DbHelper {
             SELECT
             u.id as user_id,
             bp.id as body_part_id,
+            bpt.ancestor_body_part_id,
+            bp.name as body_part_name,
             ifnull(s2.today_count,
             0) as today_count,
             ifnull(s1.total_count,
@@ -128,6 +187,9 @@ class DbHelper {
             ON
             1 = 1
           LEFT JOIN
+          	body_part_tree bpt
+          	ON bpt.descendent_body_part_id = bp.id 
+          	LEFT JOIN
               (
             SELECT
               user_id,
@@ -160,7 +222,8 @@ class DbHelper {
               ) s2
             ON
             s1.user_id = s2.user_id
-            AND s1.body_part_id = s2.body_part_id;
+            AND s1.body_part_id = s2.body_part_id
+            order by bp.display_order;
         ''');
 
         await db.execute('''
