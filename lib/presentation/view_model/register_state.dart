d4302f6139dcec2bc71bc6e5dfd3bffafffd3b79
k2491p <fukuda.dev10@gmail.com>
2022-06-13 21:51:11 +0900

    update アコーディオンに変更

---
+++ lib/presentation/view_model/register_state.dart
@@ -0,0 +1,13 @@
+// import 'package:cracking_counter/domain/entity/cracking_counter_entity.dart';
+// import 'package:freezed_annotation/freezed_annotation.dart';
+// import 'package:flutter/foundation.dart';
+//
+// part 'register_state.freezed.dart';
+//
+// @freezed
+// abstract class RegisterState with _$RegisterState {
+//   const factory RegisterState({
+//     @Default(false) bool isLoading,
+//     @Default([]) List<CrackingCounterEntity> crackingList,
+//   }) = _RegisterState;
+// }
