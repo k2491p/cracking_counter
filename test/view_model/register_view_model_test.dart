cd63738ba8ae3d66ee8011a15b5fc77b57308c4e
k2491p <fukuda.dev10@gmail.com>
2022-06-15 19:33:24 +0900

    test viewModel テストデータ取得先の変更

--- test/view_model/register_view_model_test.dart
+++ test/view_model/register_view_model_test.dart
@@ -1,12 +1,11 @@
-import 'package:cracking_counter/application/dto/cracking_counters.dart';
 import 'package:cracking_counter/application/service/cracking_counter_service.dart';
 import 'package:cracking_counter/application/shared.dart';
-import 'package:cracking_counter/domain/entity/cracking_counter_entity.dart';
 import 'package:cracking_counter/presentation/view_model/register_view_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:uuid/uuid.dart';
+import '../data/data.dart';
 import 'register_view_model_test.mocks.dart';
 
 @GenerateMocks([CrackingCounterService])
@@ -19,17 +18,12 @@ void main() {
     Shared.userId = userId;
   });
   test("一覧取得", () async {
-    var uuid = const Uuid();
-    var entity1 = CrackingCounterEntity(userId, uuid.v4(), '首', 10, 1, uuid.v4());
-    var entity2 = CrackingCounterEntity(userId, uuid.v4(), '腰', 15, 2, uuid.v4());
-    var crackingList = [entity1, entity2];
+    var crackingList = Data.GetCrackingCounterList(userId);
     when(service.getCrackingCounters()).thenAnswer((_) async => crackingList);
 
     var vm = RegisterViewModel.service(service);
     await vm.updateList();
 
     expect(vm.crackingList, crackingList);
-    expect(vm.crackingList[0].bodyPartName, '首');
-    expect(vm.crackingList[1].bodyPartName, '腰');
   });
 }
