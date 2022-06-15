68c8713644877fcd7310a7ddf1e8bcce5b2ff313
k2491p <fukuda.dev10@gmail.com>
2022-06-15 19:30:49 +0900

    test viewModel 共通処理を切り出し

--- test/view_model/register_view_model_test.dart
+++ test/view_model/register_view_model_test.dart
@@ -1,5 +1,6 @@
 import 'package:cracking_counter/application/dto/cracking_counters.dart';
 import 'package:cracking_counter/application/service/cracking_counter_service.dart';
+import 'package:cracking_counter/application/shared.dart';
 import 'package:cracking_counter/domain/entity/cracking_counter_entity.dart';
 import 'package:cracking_counter/presentation/view_model/register_view_model.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -11,9 +12,14 @@ import 'register_view_model_test.mocks.dart';
 @GenerateMocks([CrackingCounterService])
 void main() {
   final service = MockCrackingCounterService();
+  String userId = "";
+  setUp(() {
+    var uuid = const Uuid();
+    userId = uuid.v4();
+    Shared.userId = userId;
+  });
   test("一覧取得", () async {
     var uuid = const Uuid();
-    var userId = uuid.v4();
     var entity1 = CrackingCounterEntity(userId, uuid.v4(), '首', 10, 1, uuid.v4());
     var entity2 = CrackingCounterEntity(userId, uuid.v4(), '腰', 15, 2, uuid.v4());
     var crackingList = [entity1, entity2];
