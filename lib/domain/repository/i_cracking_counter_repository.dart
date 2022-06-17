ec8621b8cd796e897862a21055429412a727a3e6
k2491p <fukuda.dev10@gmail.com>
2022-06-17 22:53:20 +0900

    add 登録DB周り

--- lib/domain/repository/i_cracking_counter_repository.dart
+++ lib/domain/repository/i_cracking_counter_repository.dart
@@ -2,4 +2,5 @@ import 'package:cracking_counter/domain/entity/cracking_counter_entity.dart';
 
 abstract class ICrackingCounterRepository {
   Future<List<CrackingCounterEntity>> getAll(String userId);
+  void register(String userId, String bodyPartId);
 }
--- lib/infrastructure/sqlite/cracking_counter_sqlite.dart
+++ lib/infrastructure/sqlite/cracking_counter_sqlite.dart
@@ -1,6 +1,7 @@
 import 'package:cracking_counter/domain/entity/cracking_counter_entity.dart';
 import 'package:cracking_counter/domain/repository/i_cracking_counter_repository.dart';
 import 'package:cracking_counter/infrastructure/sqlite/db_helper.dart';
+import 'package:uuid/uuid.dart';
 
 class CrackingCounterSqlite implements ICrackingCounterRepository {
   @override
@@ -9,6 +10,19 @@ class CrackingCounterSqlite implements ICrackingCounterRepository {
     return List.generate(result.length, (i) {
       return CrackingCounterEntity(result[i]['user_id'],result[i]['body_part_id'],result[i]['body_part_name'],result[i]['total_count'],result[i]['today_count'],result[i]['ancestor_body_part_id']);
     });
+  }
+
+  @override
+  Future<void> register(String userId, String bodyPartId) async {
+    var uuid = const Uuid();
+    Map<String, String> map = {
+      'id' : uuid.v4(),
+      'user_id' : userId,
+      'body_part_id' : bodyPartId,
+      'count' : "1",
+      'register_date' : DateTime.now().toString()
+    };
+    await DbHelper.db!.insert('cracking_history', map);
   }
   
 }
