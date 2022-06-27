b95258b01608669fe4c79beb53943c1a0d130050
k2491p <fukuda.dev10@gmail.com>
2022-06-27 19:27:57 +0900

    update flutter_screenutilを導入

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:cracking_counter/presentation/component/footer.dart';
 import 'package:cracking_counter/presentation/page/register.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 Future<void> main() async {
@@ -26,21 +27,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Register(title: 'Flutter Demo Home Page'),
+    return ScreenUtilInit(
+      designSize: const Size(410, 730),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_,child) =>
+        MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: Register(title: 'Flutter Demo Home Page'),
+        ),
     );
   }
 }
